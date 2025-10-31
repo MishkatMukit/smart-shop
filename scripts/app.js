32f65dc115ef8f5855966e046a55636cf06b9674
MishkatMukit <mahabubmishkat8@gmail.com>
2025-10-31 11:24:44 +0600

    added functionality for  increase, decrease and remove items in cart

--- scripts/app.js
+++ scripts/app.js
@@ -111,22 +111,30 @@ const displayCart = ()=>{
         const cartCard = document.createElement('div')
         console.log("from cart",item)
         cartCard.innerHTML=`
-            <div class="cart-item flex items-start mb-4 pb-4 border-b border-gray-200">
+            <div class="cart-item flex items-center mb-4 pb-4 border-b border-gray-200">
                 <img src="${item.image}" alt="${item.title}" class="w-16 h-16 object-contain rounded mr-3">
-                <div class="flex-1">
-                    <h4 class="font-medium text-sm truncate">${item.title}</h4>
+                <div class="flex-1 relative">
+                    <div class="flex justify-between items-center">
+                    <h4 class=" max-w-48 font-medium text-sm overflow-hidden text-ellipsis text-nowrap">${item.title}</h4>
+                    <button 
+                                onclick="removeFromCart(${item.id})" 
+                                class="text-red-600 hover:text-red-800 mr-2 absolute -top-2 right-0 text-sm w-3 h-3 flex items-center justify-center">
+                                <i class="fa-solid fa-xmark"></i>
+                            </button>
+                    
+                    </div>
                     <div class="flex justify-between items-center mt-1">
                         <span class="text-primary font-bold text-sm">৳${(item.price * item.quantity).toFixed(2)}</span>
                         <div class="flex items-center">
                             <button 
                                 onclick="decreaseQuantity(${item.id})" 
-                                class="text-gray-500 hover:text-red-500 mr-2 text-sm w-6 h-6 flex items-center justify-center">
+                                class="text-gray-500 hover:text-red-600 mr-2 text-sm w-6 h-6 flex items-center justify-center">
                                 <i class="fas fa-minus"></i>
                             </button>
                             <span class="text-sm font-medium mx-1">${item.quantity}</span>
                             <button 
                                 onclick="increaseQuantity(${item.id})" 
-                                class="text-gray-500 hover:text-green-500 ml-2 text-sm w-6 h-6 flex items-center justify-center">
+                                class="text-gray-500 hover:text-green-600 ml-2 text-sm w-6 h-6 flex items-center justify-center">
                                 <i class="fas fa-plus"></i>
                             </button>
                         </div>
@@ -137,6 +145,35 @@ const displayCart = ()=>{
         cartItemsContainer.appendChild(cartCard)
     })
 
+}
+const increaseQuantity=(itemID)=>{
+    const item = cart.find(item=>item.id === itemID)
+    console.log(item)
+    if(item){
+        item.quantity++;
+        updateCartCount()
+        displayCart()
+    }
+}
+const decreaseQuantity=(itemID)=>{
+    const item = cart.find(item=>item.id === itemID)
+    console.log(item)
+    if(item){
+        item.quantity--;
+        if(item.quantity<1){
+            removeFromCart(itemID)
+        }
+        else{
+            updateCartCount()
+            displayCart()
+        }
+    }
+}
+const removeFromCart=(itemID)=>{
+    cart = cart.filter(item=>item.id !== itemID)
+    console.log("from remove cart ", cart)
+    updateCartCount()
+    displayCart()
 }
 const updateCartCount=()=> {
     const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
